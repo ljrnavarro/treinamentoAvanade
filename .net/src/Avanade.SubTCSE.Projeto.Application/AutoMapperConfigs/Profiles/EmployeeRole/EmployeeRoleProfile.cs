d3b8c239a42e3edf843229134b2df241f14161b0
Luiz Navarro Silva Junior PS12069904 <luiz.junior@prestadores.uhgbrasil.com.br>
2021-06-21 09:06:06 -0300

    Comit aula 21-06-21

---
+++ .net/src/Avanade.SubTCSE.Projeto.Application/AutoMapperConfigs/Profiles/EmployeeRole/EmployeeRoleProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+
+namespace Avanade.SubTCSE.Projeto.Application.AutoMapperConfigs.Profiles.EmployeeRole
+{
+    public class EmployeeRoleProfile : Profile
+    {
+        public EmployeeRoleProfile()
+        {
+            CreateMap<Dtos.EmployeeRole.EmployeeRoleDto,
+                Domain.Aggregates.EmployeeRole.Entities.EmployeeRole>()
+                .ForCtorParam("rolename", opt => opt.MapFrom(src => src.Cargo));
+
+            CreateMap<Domain.Aggregates.EmployeeRole.Entities.EmployeeRole,
+                Dtos.EmployeeRole.EmployeeRoleDto>()
+                .ForMember(dest => dest.Cargo, opt => opt.MapFrom(src => src.RoleName));
+        }
+    }
+}
--- .net/src/Avanade.SubTCSE.Projeto.Domain/Aggregates/EmployeeRole/Services/EmployeeRoleService.cs
+++ .net/src/Avanade.SubTCSE.Projeto.Domain/Aggregates/EmployeeRole/Services/EmployeeRoleService.cs
@@ -3,11 +3,12 @@ using System;
 
 namespace Avanade.SubTCSE.Projeto.Domain.Aggregates.EmployeeRole.Services
 {
-    public class EmployeeRoleService : IEmployeeRoleService
+    public interface EmployeeRoleService : IEmployeeRoleService
     {
-        void IEmployeeRoleService.AddEmployeeRole(Entities.EmployeeRole employeeRole)
+        public void AddEmployeeRol(Entities.EmployeeRole employeeRole)
         {
             throw new NotImplementedException();
         }
+      
     }
 }
