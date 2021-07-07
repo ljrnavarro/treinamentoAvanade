a5a0d6c7fe8cb9daf59face00c5cd6a1181ae1db
Luiz Navarro Silva Junior PS12069904 <luiz.junior@prestadores.uhgbrasil.com.br>
2021-07-07 08:48:30 -0300

    commit ultima aula 1

--- .net/src/Avanade.SubTCSE.Data/Repositories/Base/BaseRepository.cs
+++ .net/src/Avanade.SubTCSE.Data/Repositories/Base/BaseRepository.cs
@@ -1,5 +1,6 @@
 ﻿using Avanade.SubTCSE.Projeto.Domain.Aggregates;
 using Avanade.SubTCSE.Projeto.Domain.Base.Repository;
+using MongoDB.Driver;
 using System.Threading.Tasks;
 
 namespace Avanade.SubTCSE.Data.Repositories.Base
@@ -8,9 +9,12 @@ namespace Avanade.SubTCSE.Data.Repositories.Base
         : IBaseRepository<TEntity, Tid> where TEntity : BaseEntity<Tid>
 
     {
+        private readonly IMongoCollection<TEntity> _collection;
+        
         public virtual async Task<TEntity> Add(TEntity entity)
         {
-            throw new System.NotImplementedException();
+            await _collection.InsertOneAsync(entity);
+            return entity;
         }
 
         public virtual async Task<TEntity> FindById(Tid Id)
--- .net/src/Avanade.SubTCSE.Data/Repositories/Employee/EmployeeRepository.cs
+++ .net/src/Avanade.SubTCSE.Data/Repositories/Employee/EmployeeRepository.cs
@@ -1,8 +1,11 @@
 ﻿using Avanade.SubTCSE.Data.Repositories.Base;
+using Avanade.SubTCSE.Projeto.Domain.Aggregates.Employee.Interfaces.Repositories;
 
 namespace Avanade.SubTCSE.Data.Repositories.Employee
 {
-    public class EmployeeRepository : BaseRepository<Projeto.Domain.Aggregates.Employee.Entities.Employee, string>
+    public class EmployeeRepository : 
+        BaseRepository<Projeto.Domain.Aggregates.Employee.Entities.Employee, string>
+        ,IEmployeeRepository
     {
       
     }
